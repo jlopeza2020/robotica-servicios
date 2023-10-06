67077d62e7b97be47d91b8350c397c0db659f7d3
Julia <jlopeza2020@alumnos.urjc.es>
2023-10-06 19:52:38 +0200

    done paint cells function

--- p1.py
+++ p1.py
@@ -1,11 +1,13 @@
 import cv2
 import numpy as np
 
+NEW_WIDTH = 512
+NEW_HEIGHT = 512
 
+CELL_WIDTH = 16
+CELL_HEIGHT = 16
 
 def read_image(image):
-    # unibotics
-    #map_img = GUI.getMap('/RoboticsAcademy/exercises/static/exercises/vacuum_cleaner_loc_newmanager/resources/mapgrannyannie.png')
     # Load the image
     map_img = cv2.imread(image)
 
@@ -22,12 +24,9 @@ def resize_image(image, new_width, new_height):
 
 def draw_rectangles(image, rect_width, rect_height):  
 
-
-    # Get the dimensions of the image
-    # original size (1012,1013)
     height, width, _ = image.shape
 
-# Iterate through the image and draw rectangles
+    # Iterate through the image and draw rectangles
     for i in range(0, width, rect_width):
         for j in range(0, height, rect_height):
             # Define the top-left and bottom-right corners of the rectangle
@@ -37,57 +36,92 @@ def draw_rectangles(image, rect_width, rect_height):
             cv2.rectangle(image, pt1, pt2, (0, 255, 0), 2)
 
 
-# in the end is erode the map since it expands the values 
-# of the dark pixels 
+# dilate = erode since we want to expand black cells
 def dilate_image(image):
 
+    kernel = np.ones((5, 5), np.uint8) 
 
-    # Definir un kernel para la operación de erosión
-    kernel = np.ones((5, 5), np.uint8)  # Kernel de 15x15 con todos los elementos como 1
+    expanded_image = cv2.erode(image, kernel, iterations=1)
+    return expanded_image
 
-    # Aplicar la erosión
-    imagen_engordada = cv2.erode(image, kernel, iterations=1)
-    return imagen_engordada
+def fill_black_cells(image, image_width, image_height, cell_width, cell_height):
+    # pintar las celdas 
+
+# Dimensiones de la imagen
+#ancho_imagen = 512
+#alto_imagen = 512
+
+# Tamaño de los bloques
+#tamano_bloque = 16
+
+    color_negro = np.array([0, 0, 0])
+
+    is_black = False
+    for i in range(0, image_height, cell_height):
+        for j in range(0, image_width, cell_width):
+        # Reiniciar la bandera para cada bloque
+        #hay_pixel_negro = False
+        
+        # Comprobar si hay algún píxel en negro dentro del bloque actual
+            for x in range(i, i + cell_height):
+                for y in range(j, j + cell_width):
+
+                    if np.array_equal(color_negro,image[x][y]):
+                        is_black = True
+
+            if is_black:
+                for x in range(i, i + cell_height):
+                    for y in range(j, j + cell_width):
+
+                        image[x][y] = color_negro
+                is_black = False
+    return image
+
+        
 
 
 
+
+# in unibotics
+#map_img = GUI.getMap('/RoboticsAcademy/exercises/static/exercises/vacuum_cleaner_loc_newmanager/resources/mapgrannyannie.png')
 map = read_image('mapgrannyannie.png')
 dilated_image = dilate_image(map)
 # 800x800 is the new dimension
 resized_map = resize_image(dilated_image, 512, 512)
 
+filled_map = fill_black_cells(resized_map, 512, 512, 16, 16)
+
+
 # pintar las celdas 
 
 # Dimensiones de la imagen
-ancho_imagen = 512
-alto_imagen = 512
+#ancho_imagen = 512
+#alto_imagen = 512
 
 # Tamaño de los bloques
-tamano_bloque = 16
+#tamano_bloque = 16
 
-color_bloque  = np.array([0, 0, 255])
-color_negro = np.array([0, 0, 0])
+#color_negro = np.array([0, 0, 0])
 
-
-is_black = False
-for i in range(0, alto_imagen, tamano_bloque):
-    for j in range(0, ancho_imagen, tamano_bloque):
+#is_black = False
+#for i in range(0, alto_imagen, tamano_bloque):
+#    for j in range(0, ancho_imagen, tamano_bloque):
         # Reiniciar la bandera para cada bloque
         #hay_pixel_negro = False
         
         # Comprobar si hay algún píxel en negro dentro del bloque actual
-        for x in range(i, i + tamano_bloque):
-            for y in range(j, j + tamano_bloque):
+#        for x in range(i, i + tamano_bloque):
+#            for y in range(j, j + tamano_bloque):
 
-                if np.array_equal(color_negro,resized_map[x][y]):
-                    is_black = True
+#                if np.array_equal(color_negro,resized_map[x][y]):
+#                    is_black = True
 
-        if is_black:
-            for x in range(i, i + tamano_bloque):
-                for y in range(j, j + tamano_bloque):
+#        if is_black:
+#            for x in range(i, i + tamano_bloque):
+#                for y in range(j, j + tamano_bloque):
 
-                    resized_map[x][y] = color_negro
-            is_black = False
+#                    resized_map[x][y] = color_negro
+#            is_black = False
 
         
 
@@ -95,9 +129,9 @@ for i in range(0, alto_imagen, tamano_bloque):
 
 # 16x16 is the dimension of the cell 
 # MAS PROBABLE QUE SE DIBUJE AL FINAL 
-draw_rectangles(resized_map, 16, 16)
+draw_rectangles(filled_map, 16, 16)
 
 # in unibotics is showNumpy
-cv2.imshow('dilated map', resized_map)
+cv2.imshow('dilated map', filled_map)
 cv2.waitKey(0)
 cv2.destroyAllWindows()
