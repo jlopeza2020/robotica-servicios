f66398b0cb01d7cda4177f2147927ccd4c4be974
Julia <jlopeza2020@alumnos.urjc.es>
2023-10-08 21:59:42 +0200

    done clearing localization

--- p1_unibotics.py
+++ p1_unibotics.py
@@ -9,6 +9,13 @@ NEW_IMG_HEIGHT = 512
 CELL_WIDTH = 16
 CELL_HEIGHT = 16
 
+
+BLACK = 0
+WHITE = 127
+RED = 128
+ORANGE = 129
+
+
 def get_unibotics_map():
     # Get map and show png map
     rgba_image = GUI.getMap('/RoboticsAcademy/exercises/static/exercises/vacuum_cleaner_loc_newmanager/resources/mapgrannyannie.png')
@@ -25,10 +32,9 @@ def get_unibotics_map():
       for j in range(0, rgb_int.shape[1]):
 
         if np.array_equal(black,rgb_int[i][j]):
-          matrix[i][j] = 0
+          matrix[i][j] = BLACK
           
     return matrix
-
 
 
 def resize_image(image, new_width, new_height):
@@ -46,7 +52,7 @@ def draw_rectangles(image, rect_width, rect_height):
             pt1 = (i, j)
             pt2 = (i + rect_width, j + rect_height)
             # Draw green rectangle
-            cv2.rectangle(image, pt1, pt2, (129, 129, 129), 2)
+            cv2.rectangle(image, pt1, pt2, (ORANGE, ORANGE, ORANGE), 2)
 
 
 # dilate = erode since we want to expand black cells
@@ -61,7 +67,7 @@ def dilate_black_pixels(image):
 # Fill cells in the image with black color if any pixel in the cell is black.
 def fill_black_cells(image, image_width, image_height, cell_width, cell_height):
 
-    black = np.array([0, 0, 0])
+    #black = np.array([0, 0, 0])
 
     is_black = False
     for i in range(0, image_height, cell_height):
@@ -72,7 +78,7 @@ def fill_black_cells(image, image_width, image_height, cell_width, cell_height):
                   #print(image[x][y])
 
                     # chack if any pixel in the cell is black 
-                    if(image[x][y] == 0):
+                    if(image[x][y] == BLACK):
                         is_black = True
 
            
@@ -81,32 +87,30 @@ def fill_black_cells(image, image_width, image_height, cell_width, cell_height):
                 for x in range(i, i + cell_height):
                     for y in range(j, j + cell_width):
 
-                        image[x][y] = 0
+                        image[x][y] = BLACK
                         
 
                 is_black = False
     return image
 
+# equation obtained by linear regresion
 def get_2d_x(): 
   
     x_3d = HAL.getPose3d().x
-  
     x_2d = 17.963 - 2.915*x_3d
     
     return x_2d
   
-
+# equation obtained by linear regresion
 def get_2d_y(): 
   
     y_3d = HAL.getPose3d().y
-  
     y_2d = 13.959 + 3.092*y_3d
   
     return y_2d
 
 
-        
-print("yo8")
+
 map = get_unibotics_map()
 
 dilated_image = dilate_black_pixels(map)
@@ -132,14 +136,14 @@ while True:
     # Clear the previous cell if the position has changed
     if current_x != previous_x or current_y != previous_y:
         if previous_x is not None and previous_y is not None:
-            for aux_y in range((previous_y * 16) - 16, previous_y * 16):
-                for aux_x in range((previous_x * 16) - 16, previous_x * 16):
-                    filled_map[aux_y][aux_x] = 127  # Clear the previous cell
+            for aux_y in range((previous_y * CELL_HEIGHT) - CELL_HEIGHT, previous_y * CELL_HEIGHT):
+                for aux_x in range((previous_x * CELL_WIDTH) - CELL_WIDTH, previous_x * CELL_WIDTH):
+                    filled_map[aux_y][aux_x] = WHITE  # Clear the previous cell
 
-    # Update the current cell
-    for aux_y in range((current_y * 16) - 16, current_y * 16):
-        for aux_x in range((current_x * 16) - 16, current_x * 16):
-            filled_map[aux_y][aux_x] = 128
+    # Update the current cell in red
+    for aux_y in range((current_y * CELL_HEIGHT) - CELL_HEIGHT, current_y * CELL_HEIGHT):
+        for aux_x in range((current_x * CELL_WIDTH) - CELL_WIDTH, current_x * CELL_WIDTH):
+            filled_map[aux_y][aux_x] = RED
 
     # Show the updated map
     GUI.showNumpy(filled_map)
