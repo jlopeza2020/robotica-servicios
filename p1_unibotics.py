f8beaef77b59180a86aacf2bad187a9a32570193
Julia <jlopeza2020@alumnos.urjc.es>
2023-10-11 18:33:41 +0200

    wip class

--- p1_unibotics.py
+++ p1_unibotics.py
@@ -9,11 +9,25 @@ NEW_IMG_HEIGHT = 512
 CELL_WIDTH = 16
 CELL_HEIGHT = 16
 
+ROWS = 32
+COLS = 32
+
 
 BLACK = 0
 WHITE = 127
 RED = 128
 ORANGE = 129
+
+
+class Cell:
+    def __init__(self, x_map, y_map, x_gazebo=0, y_gazebo=0, occupied=False, cleaned=False):
+    #def __init__(self, x_map, y_map, occupied=False, cleaned=False):
+        self.x_map = x_map
+        self.y_map = y_map
+        self.x_gazebo = x_gazebo
+        self.y_gazebo = y_gazebo
+        self.ocupada = occupied  # True si está ocupada, False si no
+        self.barrida = cleaned  # True si ya ha sido barrida, False si no
 
 
 def get_unibotics_map():
@@ -65,13 +79,21 @@ def dilate_black_pixels(image):
 
 
 # Fill cells in the image with black color if any pixel in the cell is black.
+#def fill_black_cells(image, arr_cells, image_width, image_height, cell_width, cell_height):
 def fill_black_cells(image, image_width, image_height, cell_width, cell_height):
 
     #black = np.array([0, 0, 0])
-
+    
+    #arr_cells_x = 0
+    #arr_cells_y = 0
+    
     is_black = False
     for i in range(0, image_height, cell_height):
+        #arr_cells_x += 1
+        
         for j in range(0, image_width, cell_width):
+            #arr_cells_x += 1
+            #arr_cells_y += 1
         
             for x in range(i, i + cell_height):
                 for y in range(j, j + cell_width):
@@ -80,8 +102,11 @@ def fill_black_cells(image, image_width, image_height, cell_width, cell_height):
                     # chack if any pixel in the cell is black 
                     if(image[x][y] == BLACK):
                         is_black = True
+                        #arr_cells[]
 
-           
+            
+            
+            #print(arr_cells_x, arr_cells_y)
             # if exits one: paint in black the whole cell
             if is_black:
                 for x in range(i, i + cell_height):
@@ -90,6 +115,7 @@ def fill_black_cells(image, image_width, image_height, cell_width, cell_height):
                         image[x][y] = BLACK
                         
 
+                # set that it is occupied
                 is_black = False
     return image
 
@@ -118,20 +144,70 @@ def paint_cell(cell_map, x, y, cell_width, cell_height, color):
     
 
 map = get_unibotics_map()
+cells = [[Cell(x_map, y_map) for x_map in range(COLS)] for y_map in range(ROWS)]
 
 dilated_image = dilate_black_pixels(map)
 resized_map = resize_image(dilated_image, NEW_IMG_WIDTH, NEW_IMG_HEIGHT)
+#filled_map = fill_black_cells(resized_map,cells, NEW_IMG_WIDTH, NEW_IMG_HEIGHT, CELL_WIDTH, CELL_HEIGHT)
 filled_map = fill_black_cells(resized_map, NEW_IMG_WIDTH, NEW_IMG_HEIGHT, CELL_WIDTH, CELL_HEIGHT)
 
-draw_rectangles(filled_map, CELL_WIDTH, CELL_HEIGHT)
+
+####draw_rectangles(filled_map, CELL_WIDTH, CELL_HEIGHT)
+
+#check_
 
 #GUI.showNumpy(filled_map)
+#cell1 = Cell(0,0, occupied=True, =True)
+# Crear un array de 32x32 celdas inicializadas con valores predeterminados
+#filas = 32
+#columnas = 32
+
+# Crear el array de celdas
+#cells = [[Cell(x_map, y_map) for x_map in range(COLS)] for y_map in range(ROWS)]
+
+# Acceso a una celda en la posición (x_map, y_map)
+#x_map = 32  # Ejemplo de posición en el mapa en el eje x
+#y_map = 3 # Ejemplo de posición en el mapa en el eje y
+
+# Accedemos a la celda en la posición (x_map, y_map)
+#celda = cells[y_map][x_map]
+
+#celda.x_gazebo = 23
+#celda.y_gazebo = 45
+# Modificamos los atributos de la celda
+#celda.ocupada = True
+#celda.barrida = True
+
+#print(celda.x_gazebo)
+
+arr_cells_x = 0
+arr_cells_y = 0
 
 
-# Assuming you have defined filled_map and GUI previously
+for i in range(0, 512, 16):
+    #print(arr_cells_x, arr_cells_y)
+    #arr_cells_x += 1
+    #arr_cells_y += 1
+    
+    #print(arr_cells_x, arr_cells_y)
+    
+    for j in range(0, 512, 16):
+        #arr_cells_x += 1
+        #arr_cells_y += 1
+        #arr_cells_y += 1
+        if (filled_map[i][j] == BLACK):
+          celda = cells[arr_cells_x][arr_cells_y]
+          celda.ocupada = True
+          print("negro")
+        else:
+          print("blanco")
+          
+        arr_cells_y += 1
+        
+        #print(arr_cells_x, arr_cells_y)
+    arr_cells_x += 1
+    arr_cells_y = 0
 
-#previous_x = None
-#previous_y = None
 
 while True:
     # Assuming HAL.getPose3d().x and HAL.getPose3d().y provide the current position
@@ -139,47 +215,9 @@ while True:
     current_y = int(round(get_2d_y()))
     
 
-    # Clear the previous cell if the position has changed
-    #if current_x != previous_x or current_y != previous_y:
-    #    if previous_x is not None and previous_y is not None:
-    #        for aux_y in range((previous_y * CELL_HEIGHT) - CELL_HEIGHT, previous_y * CELL_HEIGHT):
-    #            for aux_x in range((previous_x * CELL_WIDTH) - CELL_WIDTH, previous_x * CELL_WIDTH):
-    #                filled_map[aux_y][aux_x] = WHITE  # Clear the previous cell
-
-    # Update the current cell in red
-    #for aux_y in range((current_y * CELL_HEIGHT) - CELL_HEIGHT, current_y * CELL_HEIGHT):
-    #    for aux_x in range((current_x * CELL_WIDTH) - CELL_WIDTH, current_x * CELL_WIDTH):
-    #        filled_map[aux_y][aux_x] = RED
-
     # Show the updated map
     paint_cell(filled_map, current_x, current_y, CELL_WIDTH, CELL_HEIGHT, RED)
     
     GUI.showNumpy(filled_map)
 
-    # Update the previous position
-    #previous_x = current_x
-    #previous_y = current_y
-
-
-#while True:
-    #print(HAL.getPose3d().x, int(round(get_2d_x())), int(round(get_2d_y())), HAL.getPose3d().y)
     
-#    get_x = int(round(get_2d_x()))
-#    get_y = int(round(get_2d_y()))
-    
-    #get_2d_x = 3
-    #get_2d_y = 4
-
-
-#    for aux_y in range(get_y*16 -16, get_y*16):
-#      for aux_x in range(get_x*16 -16, get_x*16):
-#        filled_map[aux_y][aux_x] = 128
-    
-#    GUI.showNumpy(filled_map)
-
-
-
-
-    # Enter iterative code!
-    
-  
