485a020b154f5c19ff90f20f038a930b65717c4c
Julia <jlopeza2020@alumnos.urjc.es>
2023-11-10 13:25:53 +0100

    donenavigation

--- p2/prueba.py
+++ p2/prueba.py
@@ -1,26 +1,28 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-# Función para calcular las coordenadas de la espiral de Arquímedes creciente
-def arquimedes_spiral_creciente(a, b, theta):
-    r = a + b * theta  # Ajusta según sea necesario
-    x = r * np.cos(theta)
-    y = r * np.sin(theta)
-    return x, y
+# Coordenadas iniciales
+initial_x = 40
+initial_y = -29.3
 
-# Parámetros de la espiral
-a = 0.2
-b = 0.1
+# Parámetros para la espiral
+a = 5  # Puedes ajustar este parámetro según sea necesario
+b = 0.2  # Puedes ajustar este parámetro según sea necesario
+t = np.linspace(0, 30, 10000)  # Valores de t desde 0 hasta 10
 
-# Generar puntos en la espiral
-theta_vals = np.linspace(0, 4 * np.pi, 1000)
-print(theta_vals)
-x_vals, y_vals = arquimedes_spiral_creciente(a, b, theta_vals)
+# Calcular las coordenadas de la espiral
+x = a * t * np.cos(t)
+y = a * t * np.sin(t)
 
-# Dibujar la espiral
-plt.plot(x_vals, y_vals)
-plt.title('Espiral de Arquímedes Creciente')
-plt.xlabel('X')
-plt.ylabel('Y')
-plt.grid(True)
+# Ajustar las coordenadas para iniciar desde el punto inicial
+x += initial_x
+y += initial_y
+
+# Graficar la espiral
+plt.plot(x, y, label='Espiral de Arquímedes')
+plt.scatter(initial_x, initial_y, color='red', label='Punto Inicial')
+plt.title('Espiral de Arquímedes que Comienza en el Punto Inicial')
+plt.xlabel('X-axis')
+plt.ylabel('Y-axis')
+plt.legend()
 plt.show()
