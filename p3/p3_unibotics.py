e32a8ac4763ead84c685e3775087188976950fcf
Julia <jlopeza2020@alumnos.urjc.es>
2023-11-19 18:29:13 +0100

    bebugging rectangle

--- p3/p3_unibotics.py
+++ p3/p3_unibotics.py
@@ -40,10 +40,13 @@ def parse_laser_data(laser_data):
    
 print("xxxxxxxxxxxxxxxxxxxxxxxxxxxx")
 
-alpha = 0
+#alpha = 0
 y = 1.2
-x =  2.64 
-check_free = 0
+x = 2.64 
+ocuppied = False 
+init_range = 1.0472 # 60º
+end_range = 2.0944 # 120º
+
 while True:
     front_laser = HAL.getFrontLaserData()
     parse_front_laser = parse_laser_data(front_laser)
@@ -51,27 +54,41 @@ while True:
     right_laser = HAL.getRightLaserData()
     parse_right_laser = parse_laser_data(right_laser)
     
+   # print(parse_right_laser)
+    
     back_laser = HAL.getBackLaserData()
     parse_back_laser = parse_laser_data(back_laser)
     
-    init_range, end_range = calculate_range_laser(x,y)
+    #init_range, end_range = calculate_range_laser(x, y)
     
-    #print(init_range, end_range)
-    # means 0-90
-    for i in range (len(parse_right_laser)): 
-      
-      if(init_range <= parse_right_laser[i][1] and parse_right_laser[i][1] >= end_range and parse_right_laser[i][0] != 100):
-        ocuppied = True
-        break
-      
-    if(ocuppied): 
-      print("No hay hueco")
-      ocuppied = False
-      
+    
+    #ocuppied = 100 not in [dist for dist, angle in parse_right_laser if init_range <= angle <= end_range]
 
-      
+    #print("Occupied:", ocuppied)
     
     
+    
+    #time.sleep(1)  # Add a delay to control the loop frequency
+
+    for measurement in parse_right_laser: 
+        if init_range <= measurement[1] <= end_range and measurement[0] <= 5:
+            ocuppied = True
+            break
+
+    if ocuppied:
+        print("No hay hueco")
+        ocuppied = False
+        HAL.setV(0.3)
+    else:
+        all_measurements_are_100 = all(measurement[0] > 5 for measurement in parse_right_laser)
+        if all_measurements_are_100:
+            print("Sí hay hueco")
+            HAL.setV(0.0)
+        #else:
+        #    print("Algunas medidas dentro del rango no son 100")
+            
+    
+    #.setV(0.3)
     
 
     
