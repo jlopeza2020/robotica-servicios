0c007b8cbffc1cad2ee8a40aeb7b6bb004dcc392
Julia <jlopeza2020@alumnos.urjc.es>
2023-11-30 17:22:15 +0100

    wipp3

--- p3/p3_unibotics.py
+++ p3/p3_unibotics.py
@@ -5,17 +5,29 @@ import time
 import numpy as np
 
 
-
-def comparar_lados(laser):
-    # Dividir las medidas en dos partes para representar los lados izquierdo y derecho
-    
-    
-  values = []
+def get_distances_to_car(laser):
   
+  values = []
+  counter = 0
   for dist , angle in laser:
         
     y = math.sin(angle) * dist
     values.append(y)
+    counter += 1
+    
+    
+  if (counter >= 45):
+    return values
+    
+
+def compare_sides(values):
+
+  #values = []
+  
+  #for dist , angle in laser:
+        
+    #y = math.sin(angle) * dist
+    #values.append(y)
     
   mitad = len(values) // 2
   lado_izquierdo = values[:mitad]
@@ -25,106 +37,43 @@ def comparar_lados(laser):
   suma_lado_izquierdo = sum(lado_izquierdo)
   suma_lado_derecho = sum(lado_derecho)
   
+  diff = suma_lado_izquierdo - suma_lado_derecho
+  
   print(suma_lado_izquierdo, suma_lado_derecho)
+  
+  if(abs(diff) < 15):
+    return "Alineado."
+  else: 
+    
+    if diff < 0:
+     return "girado hacia la izquierda."
+     
+    else:
+      return "girado hacia la derecha"
+  
 
   # Comparar las sumas de las medidas para determinar cuál lado es más corto
-  if suma_lado_izquierdo < suma_lado_derecho:
-    return "girado hacia la izquierda."
-  elif suma_lado_izquierdo > suma_lado_derecho:
-    return "girado hacia la derecha"
-  else:
-    return "Alineado."
+  #if suma_lado_izquierdo < suma_lado_derecho:
+  #  return "girado hacia la izquierda."
+  #elif suma_lado_izquierdo > suma_lado_derecho:
+  #  return "girado hacia la derecha"
+  #else:
+  #  return "Alineado."
 
 
-def calcular_dispersion(right_laser):
+def calcular_dispersion(values):
   
-  values = []
+  #values = []
   
-  for dist , angle in right_laser:
+  #for dist , angle in right_laser:
         
-    #x = math.cos(angle) * dist
-    y = math.sin(angle) * dist
+  #  y = math.sin(angle) * dist
    
-    #if(math.radians(0) < angle < math.radians(180)):
-    values.append(y)
+    #values.append(y)
 
-  #x_values = np.array([p[0] for p in values])
-  #y_values = np.array([p[1] for p in values])
-  
-  #pendiente = np.diff(y_values) / np.diff(x_values)
-
- 
   return np.std(values)
 
 
-"""
-def calcular_pendiente(right_laser):
-  
-  values = []
-  
-  for dist , angle in right_laser:
-        
-    x = math.cos(angle) * dist
-    y = math.sin(angle) * dist
-   
-    if(math.radians(0) < angle < math.radians(180)):
-      values.append((x,y))
-
-  x_values = np.array([p[0] for p in values])
-  y_values = np.array([p[1] for p in values])
-  
-  pendiente = np.diff(y_values) / np.diff(x_values)
-
- 
-  return np.mean(pendiente)
-"""
-"""
-def get_back_straight(back_laser):
-  
-  is_turned = False
-  times = 0
-  
-  for dist , angle in back_laser:
-    
-    if (angle <= math.pi/2):
-      if(dist != 100): 
-        times += 1
-        
-      
-  
-  for dist , angle in back_laser:
-    
-    if (angle <= math.pi/2):
-      if(dist != 100): 
-        is_turned = True
-        break
-
-  return is_turned , times
-
-"""
-"""
-def get_front_straight(front_laser):
-  
-  is_turned = False
-  times = 0
-  for dist , angle in front_laser:
-    
-    if (angle >= math.pi/2):
-      
-      if(dist != 100): 
-        times += 1
-  
-  for dist , angle in front_laser:
-    
-    if (angle >= math.pi/2):
-      
-      if(dist != 100): 
-        is_turned = True
-        break
-
-  return is_turned , times
-  
-"""   
 def get_betha(a, b):
   
   h = math.sqrt(pow(a/2, 2) + pow(b,2))
@@ -144,10 +93,9 @@ def parse_laser_data(laser_data):
         # if value is inside of the minimum and maximun 
         if(laser_data.minRange < laser_data.values[i] and laser_data.values[i] < laser_data.maxRange):
           dist = laser_data.values[i]
-        #else: 
-        #  dist = 100 # means that 100 = inf
           angle = math.radians(i)
           laser += [(dist, angle)]
+          
     return laser
    
 print("xxxxxxxxxxxxxxxxxxxxxxxxxxxx")
@@ -174,7 +122,7 @@ while True:
     right_laser = HAL.getRightLaserData()
     parse_right_laser = parse_laser_data(right_laser)
     
-    #get_distances_to_car(parse_right_laser)
+    distances_cars = get_distances_to_car(parse_right_laser)
     
     #back_laser = HAL.getBackLaserData()
     #parse_back_laser = parse_laser_data(back_laser)
@@ -182,26 +130,13 @@ while True:
     # STATE 1: ALIGN 
     if (align): 
       
-
-      #is_turned_front, amount_fl_af = get_front_straight(parse_front_laser)
-      #is_turned_back, amount_bl_af = get_back_straight(parse_back_laser)
+      dis = calcular_dispersion(distances_cars)
       
-      #pte = calcular_pendiente(parse_right_laser)
-      
-      dis = calcular_dispersion(parse_right_laser)
-      
-      if first_iter:
-
-        #init_pte = calcular_pendiente(parse_right_laser) 
-        init_dis = calcular_dispersion(parse_right_laser)
-      
-        first_iter = False  
+      #if first_iter:
+      #  init_dis = calcular_dispersion(distances_cars)
+      #  first_iter = False  
       
       
-      #if (init_pte -0.05 < pte < init_pte + 0.05):
-      #  print("alineado")
-      #  HAL.setW(0.0)
-        
       # solo tener en cuenta si hay un mínimo de 40 medidas 
       #counter = 0
       #for d_actual, angle in parse_right_laser:
@@ -209,26 +144,19 @@ while True:
       
       #print(counter)
       #if(counter >= 40):
-      resultado = comparar_lados(parse_right_laser)
+      resultado = compare_sides(distances_cars)
       print(resultado)  
       
-      print(init_pte, pte, HAL.getPose3d().yaw, init_dis, dis)
+      #print(HAL.getPose3d().yaw, init_dis, dis)
+      #print(init_pte, pte, HAL.getPose3d().yaw, init_dis, dis)
      
      
-      
       # if yaw es positiva: está girada hacia la izq 
       # si yaw es negativa está girada a la derecha 
       
-      # need to robust movement and set threshold 
-      # start moving until threshold 
-      #if(is_turned_back and is_turned_front == False):
-      #  print("is turn back and amount " + str(amount_bl_af))
-      #  HAL.setW(-0.25)
-        
-      #if(is_turned_front and is_turned_back == False):
-      #  print("is turn front and amount "+ str(amount_fl_af))
-      #  HAL.setW(0.25)
-        
+      
+      
+      
         
      # STATE 2: FIND SPACE
     if (find):
