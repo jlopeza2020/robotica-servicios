c5a34afff587de9b144046e91af4887480591dbf
Julia <jlopeza2020@alumnos.urjc.es>
2024-01-02 19:44:57 +0100

    fixed obstacles

--- p4/ejemplo.py
+++ p4/ejemplo.py
@@ -1,4 +1,5 @@
 import cv2
+import numpy as np
 
 # Load the image from the file
 image = cv2.imread('/home/juloau/Desktop/CUARTO/PRIMER_CUATRI/ROBOTICA_SERVICIOS/REPO/robotica-servicios/p4/map.png')
@@ -9,12 +10,20 @@ if image is not None:
     height, width, _ = image.shape
 
     # Iterate over each pixel in the image
-    for i in range(height):
-        for j in range(width):
-            print(image[i, j])
+    #for i in range(height):
+    #    for j in range(width):
+            #print(image[i, j])
+
+
+    # Define el kernel o elemento estructurante para la dilatación
+    kernel = np.ones((10,10), np.uint8)  # Puedes ajustar el tamaño del kernel según tus necesidades
+
+    # Aplica la dilatación a la imagen
+    cv2.erode(image, kernel, iterations=1)
 
     # Optionally, display the image
     cv2.imshow('Image', image)
+    #cv2.imshow('DELATTED IMAGE', dilated_image)
     cv2.waitKey(0)
     cv2.destroyAllWindows()
 
--- p4/p4_unibotics.py
+++ p4/p4_unibotics.py
@@ -29,7 +29,8 @@ def extract_obstacles(image):
         for j in range(width):
       
             if image[i][j][0] < 0.5 and image[i][j][1] < 0.5  and image[i][j][2] < 0.5:
-                obstacles.append([i, j])
+                # x, y ,radius 
+                obstacles.append([i, j, 1])
     return obstacles
 
 # from meter to pixel 
@@ -40,20 +41,20 @@ def rw2map(x_rw, y_rw):
 
 def isStateValid(state):
   
-    #print(state)
     x = state.getX()
-    #print(x)
     y = state.getY()
-    #print(y)
 
     # Check if the state is inside any obstacle
     for obstacle in obstacles:
-        #print(rw2map(*obstacle))
-        obstacle_x, obstacle_y = rw2map(*obstacle)
-        distance = sqrt(pow(x - obstacle_x, 2) + pow(y - obstacle_y, 2))
+        #obstacle_x, obstacle_y = rw2map(*obstacle)
+        #distance = sqrt(pow(x - obstacle_x, 2) + pow(y - obstacle_y, 2))
+      
 
-        if distance < 0.8:  
-            return False
+
+        #if distance < 0.8:  
+        #    return False
+        if sqrt(pow(x - obstacle[0], 2) + pow(y - obstacle[1], 2)) - obstacle[2] <= 0:
+          return False
     return True
 
 def plan():
@@ -93,33 +94,11 @@ def plan():
     pdef.setStartAndGoalStates(start, goal)
 
     # Create a planner for the defined space
-    # tiene muchos que se quedan en los obstáculos 
+    #planner = og.EST(si)
+    planner = og.PRM(si)
     #planner = og.RRTConnect(si)
-    # solo da el punto de entrada y salida
-    #planner = og.PRM(si)
-    # no todos los puntos llegan al objetivo
-    ##planner = og.EST(si)
-    # MALLL 
-    #planner = og.KPIECE1(si)
-    # NO CAMBIA SOLO PRINCIPIO Y FIN
-    #planner = og.LazyPRM(si)
-    # MUCHOS PUNTOS Y CAMINO MUY ENREVESADO
-    #planner = og.SBL(si)
-    # NO VALID SOLUTION FOUND 
-    #planner = og.FMT(si)
-    # PUNTO DE INICIO Y FINAL
-    #planner = og.ABITstar(si)
-    #planner = og.PRMstar(si)
-    # NO LLEGA AL OBJETIVO
-    #planner = og.InformedRRTstar(si)
+
     
-
-
-
-
-
-
-
     # Set the problem we are trying to solve for the planner
     planner.setProblemDefinition(pdef)
 
@@ -127,16 +106,30 @@ def plan():
     planner.setup()
 
     # Solve the problem and print the solution if it exists
+    #solved = planner.solve(1.0)
+    #if solved:
+    #    solution_path = pdef.getSolutionPath()
+    #    matrix = solution_path.printAsMatrix()
+    #    path = create_numpy_path(matrix)
+    #    return path
+    #else: 
+    #    print("NO SOLUTION FOUND")
+    
+    # ... (previous code)
+
+    # Solve the problem and print the solution if it exists
     solved = planner.solve(1.0)
     if solved:
-        solution_path = pdef.getSolutionPath()
-        matrix = solution_path.printAsMatrix()
-        path = create_numpy_path(matrix)
-        #print(path)
-        return path
+      solution_path = pdef.getSolutionPath()
+      if solution_path:
+          matrix = solution_path.printAsMatrix()
+          path = create_numpy_path(matrix)
+          return path
+      else:
+          print("NO SOLUTION FOUND: getSolutionPath returned None")
     else: 
-        print("NO SOLUTION FOUND")
-        
+      print("NO SOLUTION FOUND: Planner failed to solve")
+    
 
 def create_numpy_path(states):
     lines = states.splitlines()
@@ -152,53 +145,30 @@ def create_numpy_path(states):
 rgb_image = GUI.getMap('/RoboticsAcademy/exercises/static/exercises/amazon_warehouse_newmanager/resources/images/map.png')
 
 
-#kernel = np.ones((5, 5), np.uint8) 
+kernel = np.ones((7, 7), np.uint8) 
 # Thick obstacles
-#rgb_image = cv2.erode(rgb_image, kernel, iterations=1)
+rgb_image = cv2.erode(rgb_image, kernel, iterations=1)
 
 # Set dimensions in pixels
 dimensions = [0, 0, 279, 415] 
 # Extract obstacles from the image
 obstacles = extract_obstacles(rgb_image)
-# Plan the path
+
 """
+# Plan the path
 solution_path = plan()
 
-
-# expected path to obtain
-# init point: [206,140] -> [0,0]
-# intermediate point : [196,90]
-# goal point: [197,62] -> [3.728, 0.579]
-
-#print(HAL.getPose3d().x,HAL.getPose3d().y)
-
-y_init, x_init = rw2map(0,0) 
-y_objective, x_objective = rw2map(3.728,0.579)
-
-inverted_solution = invert_array(solution_path)
-print(inverted_solution)
-
-GUI.showPath(inverted_solution)
+if solution_path is not None:
+  inverted_solution = invert_array(solution_path)
+  print(inverted_solution)
+  GUI.showPath(inverted_solution)
+else:
+  print("No valid solution found.")
 """
-
 while True:
-    #HAL.setV(3)
-    #print(HAL.getPose3d().x,HAL.getPose3d().y)
     
-    #obstacles = extract_obstacles(rgb_image)
-    # Plan the path
     solution_path = plan()
 
-
-    # expected path to obtain
-    # init point: [206,140] -> [0,0]
-    # intermediate point : [196,90]
-    # goal point: [197,62] -> [3.728, 0.579]
-
-    #print(HAL.getPose3d().x,HAL.getPose3d().y)
-
-    #y_init, x_init = rw2map(0,0) 
-    #y_objective, x_objective = rw2map(3.728,0.579)
     if solution_path is not None:
         inverted_solution = invert_array(solution_path)
         print(inverted_solution)
@@ -206,7 +176,3 @@ while True:
     else:
       print("No valid solution found.")
     
-    #inverted_solution = invert_array(solution_path)
-    #print(inverted_solution)
-    
-    #GUI.showPath(inverted_solution)
