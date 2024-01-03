acfc45beaf9c47cb1e0a6815fb47dcb482161a75
Julia <jlopeza2020@alumnos.urjc.es>
2024-01-03 20:12:25 +0100

    stated while path is not noe

--- p4/p4_unibotics.py
+++ p4/p4_unibotics.py
@@ -10,6 +10,13 @@ from math import sqrt
 import numpy as np
 import matplotlib.pyplot as plt
 
+SHX = 3.728
+SH1Y = 0.579
+SH2Y = -1.242
+SH3Y = -3.039
+SH4Y = -4.827 
+SH5Y = -6.781
+SH6Y = -8.665
 
 def invert_array(array):
   
@@ -38,6 +45,11 @@ def rw2map(x_rw, y_rw):
     x_map = 139.5 - 20.515 * x_rw
     y_map = 207.5 - 20.16 * y_rw
     return (round(y_map), round(x_map))
+ 
+def map2rw(x_map, y_map):
+    x_rw =  6.8 -0.049* x_map
+    y_rw =  10.310 -0.05* y_map
+    return (round(y_rw), round(x_rw))
 
 def isStateValid(state):
   
@@ -46,13 +58,7 @@ def isStateValid(state):
 
     # Check if the state is inside any obstacle
     for obstacle in obstacles:
-        #obstacle_x, obstacle_y = rw2map(*obstacle)
-        #distance = sqrt(pow(x - obstacle_x, 2) + pow(y - obstacle_y, 2))
-      
-
-
-        #if distance < 0.8:  
-        #    return False
+        
         if sqrt(pow(x - obstacle[0], 2) + pow(y - obstacle[1], 2)) - obstacle[2] <= 0:
           return False
     return True
@@ -75,7 +81,7 @@ def plan():
     si.setStateValidityChecker(ob.StateValidityCheckerFn(isStateValid))
 
     y_init, x_init = rw2map(0,0) 
-    y_objective, x_objective = rw2map(3.728,0.579)
+    y_objective, x_objective = rw2map(SHX,SH1Y)
     
     # Set our robot's starting and goal state
     start = ob.State(space)
@@ -98,25 +104,12 @@ def plan():
     planner = og.PRM(si)
     #planner = og.RRTConnect(si)
 
-    
     # Set the problem we are trying to solve for the planner
     planner.setProblemDefinition(pdef)
 
     # Perform setup steps for the planner
     planner.setup()
-
-    # Solve the problem and print the solution if it exists
-    #solved = planner.solve(1.0)
-    #if solved:
-    #    solution_path = pdef.getSolutionPath()
-    #    matrix = solution_path.printAsMatrix()
-    #    path = create_numpy_path(matrix)
-    #    return path
-    #else: 
-    #    print("NO SOLUTION FOUND")
     
-    # ... (previous code)
-
     # Solve the problem and print the solution if it exists
     solved = planner.solve(1.0)
     if solved:
@@ -154,19 +147,22 @@ dimensions = [0, 0, 279, 415]
 # Extract obstacles from the image
 obstacles = extract_obstacles(rgb_image)
 
-"""
-# Plan the path
-solution_path = plan()
 
-if solution_path is not None:
-  inverted_solution = invert_array(solution_path)
-  print(inverted_solution)
-  GUI.showPath(inverted_solution)
-else:
-  print("No valid solution found.")
-"""
+solution_path = None
+while solution_path is None: 
+  solution_path = plan()
+
+  if solution_path is not None:
+    inverted_solution = invert_array(solution_path)
+    print(inverted_solution)
+    GUI.showPath(inverted_solution)
+  else:
+    print("No valid solution found.")
+
+
+
 while True:
-    
+    """
     solution_path = plan()
 
     if solution_path is not None:
@@ -175,4 +171,10 @@ while True:
         GUI.showPath(inverted_solution)
     else:
       print("No valid solution found.")
+    """
+    ## vaya al objetivo 
+    ## coja la estantería 
+    ## vaya la destino (ya cambia su dimensión) 
+    ##  deje la estantería 
+    
     
